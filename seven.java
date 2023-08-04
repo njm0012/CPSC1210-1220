e9c768bef66f3261f7e4a437ad1f66f3f84f50ad
Nick Mitchell <133914800+njm0012@users.noreply.github.com>
2023-08-04 00:33:30 +0000

    Conditional Code practice. Finished.

---
+++ seven.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; // command used to import scanner tool
+public class seven {
+    public static void main (String[] args){
+        System.out.println("Give a number:");
+        Scanner scanner = new Scanner(System.in); // Reads user import
+
+        int number = Integer.valueOf(scanner.nextLine()); // user input
+        if (number >= 1984) { // Conditional
+            System.out.println("Orwell");
+        }
+    }
+}
